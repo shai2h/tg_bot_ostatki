83f19f8d6475b4a878c09b6fd755c5cdaa099073
Ilfat Shaydullin <sweden.fan@yandex.ru>
2025-05-15 14:47:04 +0300

    updated env variable

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 import sys
 from os.path import abspath, dirname
 
+from app.config import settings
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -12,14 +13,14 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 
 # Импортируем модели
-from db.database import Base, DATABASE_URL
+from db.database import Base
 from app.warehouse_stock.models import WarehouseStocks
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
