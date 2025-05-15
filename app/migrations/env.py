4231b9cade2f6213b00da951bde0ff4f602f2bc0
Ilfat Shaydullin <sweden.fan@yandex.ru>
2025-05-15 11:47:22 +0300

    fixed

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -13,7 +13,7 @@ sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 # Импортируем модели
 from db.database import Base, DATABASE_URL
-from app.warehouse_stock.models import Hotels
+from app.warehouse_stock.models import WarehouseStocks
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
