7dc3ccf5dd0435f84021c277d973cbc23d7c4abc
Ilfat Shaydullin <sweden.fan@yandex.ru>
2025-05-16 11:52:46 +0300

    updated router api/ostatki

--- app/api/ostatki.py
+++ app/api/ostatki.py
@@ -4,10 +4,12 @@ from app.db.database import async_session_maker
 from app.warehouse_stock.models import WarehouseStocks
 from sqlalchemy import insert
 from typing import List, Dict, Any
-
+from sqlalchemy.dialects.postgresql import insert
 
 router = APIRouter()
 
+
+from sqlalchemy.dialects.postgresql import insert
 
 @router.post("/api/ostatki")
 async def receive_ostatki(data: List[Dict[str, Any]] = Body(...)):
@@ -15,8 +17,20 @@ async def receive_ostatki(data: List[Dict[str, Any]] = Body(...)):
         async with async_session_maker() as session:
             for item in data:
                 stmt = insert(WarehouseStocks).values(**item)
+                stmt = stmt.on_conflict_do_update(
+                    index_elements=["kod", "sklad"],  # ключ и склад по уникальным
+                    set_={
+                        "ostatok": item["ostatok"],
+                        "price": item["price"],
+                        "name": item["name"],
+                        "vid": item["vid"],
+                        "brend": item["brend"],
+                        "articul": item.get("articul"),
+                    }
+                )
                 await session.execute(stmt)
             await session.commit()
-        return {"status": "ok", "inserted": len(data)}
+        return {"status": "ok", "processed": len(data)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
