fca5c1754b6fed5de0be4f77cf25afb8ce2e85e9
Ilfat Shaydullin <sweden.fan@yandex.ru>
2025-05-16 11:53:19 +0300

    updated int -> str

--- app/warehouse_stock/models.py
+++ app/warehouse_stock/models.py
@@ -1,6 +1,8 @@
-from db.database import Base
-from sqlalchemy import Column, Integer, JSON, String
+from app.db.database import Base
+from sqlalchemy import Column, Integer, String
 
+
+from sqlalchemy import UniqueConstraint
 
 class WarehouseStocks(Base):
     __tablename__ = "warehouse_stock"
@@ -10,7 +12,11 @@ class WarehouseStocks(Base):
     name = Column(String, nullable=False)
     vid = Column(String)
     brend = Column(String)
-    kod = Column(Integer)
-    price = Column(Integer)
-    ostatok = Column(Integer)
+    kod = Column(String)      # ← строка, как ты указал
+    price = Column(String)
+    ostatok = Column(String)
     sklad = Column(String)
+
+    __table_args__ = (
+        UniqueConstraint("kod", "sklad", name="uix_kod_sklad"),
+    )
