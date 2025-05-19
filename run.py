00902a7d83ee032d5884e457de3842ba67115877
Ilfat Shaydullin <sweden.fan@yandex.ru>
2025-05-19 08:44:34 +0300

    updated

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-from app.bot.main import main
+import asyncio
+from uvicorn import Config, Server
+from app.api.main import app
+from app.bot.main import main as run_bot
 
-if __name__ == '__main__':
-    main()
+async def start_fastapi():
+    server = Server(Config(app=app, host="0.0.0.0", port=8000, reload=False))
+    await server.serve()
+
+async def start_all():
+    await asyncio.gather(
+        run_bot(),
+        start_fastapi(),
+    )
+
+if __name__ == "__main__":
+    asyncio.run(start_all())
